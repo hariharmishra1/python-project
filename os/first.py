87323e2ccbff53c9a19f0459f780f73634e1a033
Harihar <harihar.mishra.ext@nokia.com>
2024-08-08 14:09:58 +0530

    added os modules

---
+++ os/first.py
@@ -0,0 +1,9 @@
+import os
+cwd = os.getcwd()
+print ("print current working directory", cwd)
+files = os.listdir ()
+print(files)
+chdir = os.chdir(r'C:\Users\hmishra\OneDrive - Nokia\Python_by_harihar\python-project\os')
+print (chdir)
+files =os.listdir()
+print(files)
